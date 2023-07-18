e315ab7a75fd1470881222eaddd7d37e53285f73
Bertrand Tang <bertrand.tang.pro@gmail.com>
2023-07-18 11:16:23 +0200

    minors modifications, added template for media queries

--- public/js/buttons.js
+++ public/js/buttons.js
@@ -1,9 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
     const button = document.querySelector('.minimize');
+  //  const button2 = document.querySelector('.maximize');
+
     const audio = new Audio("../mp3/dry-fart.mp3")
+  //  const audio2 = new Audio("../mp3/never-gonna-give-you-up.m4a")
+
     button.addEventListener('click', function() {
         audio.play();
         confirm(`💨 ... Mais t'as pété ? 👀`);
-        
     });
+
+ /*   button2.addEventListener('click', function() {
+        audio2.play();
+    }); */
 });
